b8ba1d16fb0524406dd5014591e00087d3928307
thrashr888 <thrashr888@gmail.com>
2014-05-13 22:48:42 -0700

    support multiple notes

---
+++ app/scripts/reactTest.jsx
@@ -0,0 +1,43 @@
+
+/** @jsx React.DOM */
+React.renderComponent(
+  <p>Hello, world!</p>,
+  document.getElementById('o6')
+);
+
+/** @jsx React.DOM */
+var TodoList = React.createClass({
+  render: function() {
+    var createItem = function(itemText) {
+      return <li>{itemText}</li>;
+    };
+    return <ul>{this.props.items.map(createItem)}</ul>;
+  }
+});
+var TodoApp = React.createClass({
+  getInitialState: function() {
+    return {output: [], input: ''};
+  },
+  onChange: function(e) {
+    this.setState({text: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var nextItems = this.state.items.concat([this.state.text]);
+    var nextText = '';
+    this.setState({items: nextItems, text: nextText});
+  },
+  render: function() {
+    return (
+      <div>
+        <h3>TODO</h3>
+        <TodoList items={this.state.items} />
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.onChange} value={this.state.text} />
+          <button>{'Add #' + (this.state.items.length + 1)}</button>
+        </form>
+      </div>
+    );
+  }
+});
+React.renderComponent(<TodoApp />, mountNode);
