7b6711dd475150fe61f2d5fd0f6b1d2292a061fb
thrashr888 <thrashr888@gmail.com>
2014-06-14 01:08:46 -0700

    Apply ES6 => to EditorOutput module to test.

--- app/scripts/components/EditorOutput.jsx
+++ app/scripts/components/EditorOutput.jsx
@@ -11,11 +11,12 @@ var EditorOutputLine = require('./EditorOutputLine.jsx');
 var EditorOutput = React.createClass({
   render: function () {
     // console.log(this.props.output);
-    var lineList = this.props.output ? this.props.output.map(function (text, i) {
-      return <EditorOutputLine text={text} key={i}/>;
-    }) : [];
     return (
-      <div className={this.props.className + ' m-note--output'}>{lineList}</div>
+      <div className={this.props.className + ' m-note--output'}>
+        {this.props.output ? this.props.output.map((text, i) =>
+          <EditorOutputLine text={text} key={i}/>
+        ) : null}
+      </div>
     );
   }
 });
